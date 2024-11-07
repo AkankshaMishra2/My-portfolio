cd4995054a358cb74f318b40e7d83978eea3336f
AkankshaMishra2 <akankshamishra20042@gmail.com>
2024-11-07 15:52:36 +0530

    Deleted All Files : (

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [], // Add any external image domains here if needed
-  },
-}
-
-module.exports = nextConfig
